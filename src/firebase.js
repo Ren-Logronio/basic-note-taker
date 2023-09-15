33340cdb1564f9100aaf947c71e1ffe4953e4a9e
Ren-Logronio <reinhart.logronio@msugensan.edu.ph>
2023-09-16 03:09:50 +0800

    final

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-analytics.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-firestore.js";
+import { getFirestore, collection, query, where, getDocs } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-firestore.js";
 import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-auth.js";
 import { GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-auth.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-.
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -27,6 +27,18 @@ const provider = new GoogleAuthProvider();
 const db = getFirestore(app);
 const auth = getAuth();
 auth.languageCode = 'it';
+const notesRef = collection(db, 'notes');
+let userId = null;
+let user = null;
+
+const liClass = "list-group-item list-group-item-action d-flex justify-content-between".split(/\s+/);
+const pClass = "h6 text-start align-self-center m-0 p-0".split(/\s+/);
+const divClass = "d-flex justify-content-end".split(/\s+/);
+const aDeleteClass = "ms-1 btn btn-outline-danger d-flex justify-center align-center".split(/\s+/);
+
+const noteTitleInput = document.querySelector('#note-title-input');
+const noteContentInput = document.querySelector('#note-content-input');
+const addNoteButton = document.querySelector('#add-note-button');
 
 const signinCard = document.querySelector('#signin-card');
 const signoutCard = document.querySelector('#signout-card');
@@ -35,6 +47,9 @@ const emailHolder = document.querySelector('#email-holder');
 const googleSignoutButton = document.querySelector('#google-signout-button');
 const homeHandle = document.querySelector('#home-handle');
 const dashHandle = document.querySelector('#dash-handle');
+const noteDash = document.querySelector('#note-dash');
+const noteList = document.querySelector('#note-list');
+const noteEmptyHandle = document.querySelector('#note-empty-handle');
 
 // check if signed in
 onAuthStateChanged(auth, (user) => {
@@ -46,11 +61,30 @@ onAuthStateChanged(auth, (user) => {
         if(!signinCard.classList.contains('d-none')) signinCard.classList.add('d-none');
         if(!homeHandle.classList.contains('d-none')) dashHandle.classList.add('d-none');
         emailHolder.innerHTML = user.email;
+        userId = user.uid;
+
+        // get notes
+        const q = query(notesRef, where("uid", "==", user.uid));
+        getDocs(q).then((querySnapshot) => {
+            let notes = [];
+            querySnapshot.forEach((doc) => {
+                // doc.data() is never undefined for query doc snapshots
+                notes.push(doc.data());
+            });
+            if(notes.length > 0){
+                if(!noteEmptyHandle.classList.contains('d-none')) noteEmptyHandle.classList.add('d-none');
+                appendLists(notes);
+            }
+        }).catch((error) => {
+            console.log("Error getting documents: ", error);
+        });
     } else {
         signinCard.classList.remove('d-none');
         homeHandle.classList.remove('d-none');
+        if(!noteDash.classList.contains('d-none')) noteDash.classList.add('d-none');
         if(!dashHandle.classList.contains('d-none')) dashHandle.classList.add('d-none');
         if(!signoutCard.classList.contains('d-none')) signoutCard.classList.add('d-none');
+        userId = null;
         console.log('not signed in');
     }
 });
@@ -60,7 +94,6 @@ googleSignoutButton.addEventListener('click', ()=>{
         // Sign-out successful.
         console.log('signed out');
         signoutCard.classList.add('d-none');
-        document.cookie = JSON.stringify({token: '', user: ''});
         emailHolder.innerHTML = '';
         signout
     }).catch((error) => {
@@ -76,7 +109,7 @@ googleSigninButton.addEventListener('click', ()=>{
         const credential = GoogleAuthProvider.credentialFromResult(result);
         const resultToken = credential.accessToken;
         const resultUser = result.user;
-        document.cookie = JSON.stringify({token: `${resultToken}`, user: resultUser});
+        user = resultUser;
         // The signed-in user info.
         // IdP data available using getAdditionalUserInfo(result)
         // ...
@@ -95,3 +128,113 @@ googleSigninButton.addEventListener('click', ()=>{
         // ...
     });
 });
+
+function reloadList(){
+    // re-query list
+    const q = query(notesRef, where("uid", "==", userId));
+    getDocs(q).then((querySnapshot) => {
+        let notes = [];
+        querySnapshot.forEach((doc) => {
+            // doc.data() is never undefined for query doc snapshots
+            notes.push(doc.data());
+        });
+        if(notes.length > 0){
+            if(!noteEmptyHandle.classList.contains('d-none')) noteEmptyHandle.classList.add('d-none');
+            appendLists(notes);
+        }
+    }).catch((error) => {
+        console.log("Error getting documents: ", error);
+    });
+};
+
+function appendToList(id, title) {
+    let li = document.createElement('li');
+    li.id = id;
+    li.classList.add(...liClass);
+    
+    let p = document.createElement('p');
+    p.classList.add(...pClass);
+    p.innerHTML = title;
+    
+    let div = document.createElement('div');
+    div.classList.add(...divClass);
+
+    let aDelete = document.createElement('a');
+    aDelete.addEventListener('click', ()=>{
+        removeList(id);
+    });
+    aDelete.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16"><path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/><path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/></svg>`;
+    aDelete.classList.add(...aDeleteClass);
+
+    div.appendChild(aDelete);
+
+    li.appendChild(p);
+    li.appendChild(div);
+
+    noteList.appendChild(li);
+}
+
+function appendLists(notes){
+    notes.forEach(note => {
+        appendToList(note.id, note.title);
+    });
+}
+
+function removeList(id){
+    // delete note from firestore
+    const q = query(notesRef, where("id", "==", id));
+    getDocs(q).then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            // doc.data() is never undefined for query doc snapshots
+            deleteDoc(doc.ref);
+        });
+        // remove note from list
+        document.getElementById(id).remove();
+        // check if list is empty
+        reloadList();
+    }).catch((error) => {
+        console.log("Error getting documents: ", error);
+    });
+}
+
+function addNote(){
+    if(userId){
+        // add note to firestore
+        const noteId = create_UUID();
+        const noteTitle = noteTitleInput.value;
+        const noteContent = noteContentInput.value;
+        const date = new Date();
+        const unixGetTime = Math.floor(date.getTime() / 1000);
+        const note = {
+            id: noteId,
+            title: noteTitle,
+            content: noteContent,
+            date: unixGetTime,
+            uid: userId
+        };
+        addDoc(notesRef, note).then(()=>{
+            // add note to list
+            appendToList(noteId, noteTitle);
+        }).catch((error)=>{
+            console.log(error);
+            alert('Error adding note');
+        });
+    } else {
+        console.log('not signed in');
+    }
+}
+
+function create_UUID(){
+    var dt = new Date().getTime();
+    var uuid = 'notes-xxxx-xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (dt + Math.random()*16)%16 | 0;
+        dt = Math.floor(dt/16);
+        return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+}
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    addNoteButton.addEventListener('click', ()=>{ addNote(); });
+});
+
